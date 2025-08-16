4eecdd449e97d85c11ffe3a945124ddff1cb0bf3
samthambad <98635284+samthambad@users.noreply.github.com>
2025-08-16 16:24:35 +0800

    Added list functionality

--- src/main/java/Sisyphus.java
+++ src/main/java/Sisyphus.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Sisyphus {
     public static void main(String[] args) {
         String logo = """
@@ -15,16 +16,26 @@ public class Sisyphus {
         System.out.println(logo);
         System.out.println(welcomeMessage);
         System.out.println(divider);
+        ArrayList<String> todoList = new ArrayList<String>();
         String input = "";
         Scanner myObj = new Scanner(System.in);
         while (!input.equals("bye")) {
           input = myObj.nextLine();
           System.out.println(divider);
-          if (input.equals("bye")) {
-            System.out.println("    See you!");
-          }
-          else {
-          System.out.println("    " +input);
+          switch (input) {
+            case "bye":
+              System.out.println("    See you!");
+              break;
+            case "list":
+              int i = 1;
+              for (String item : todoList) {
+                System.out.println("    " + i++ + ". " + item);
+              }
+              break;
+            default:
+              todoList.add(input);
+              System.out.println("    added: " + input);
+              break;
           }
           System.out.println(divider);
         }
