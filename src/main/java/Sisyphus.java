3b1d53c5b71efd814603ac59da372a089869de3e
samthambad <98635284+samthambad@users.noreply.github.com>
2025-08-16 16:12:47 +0800

    echo code

--- src/main/java/Sisyphus.java
+++ src/main/java/Sisyphus.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Sisyphus {
     public static void main(String[] args) {
         String logo = """
@@ -9,8 +10,23 @@ public class Sisyphus {
               ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░ 
         ░▒▓███████▓▒░░▒▓█▓▒░▒▓███████▓▒░   ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓███████▓▒░  
           """;
+        String divider = "-----------------------------";
         String welcomeMessage = "Hello, I am Sisyphus, what can I do for you?";
         System.out.println(logo);
         System.out.println(welcomeMessage);
+        System.out.println(divider);
+        String input = "";
+        Scanner myObj = new Scanner(System.in);
+        while (!input.equals("bye")) {
+          input = myObj.nextLine();
+          System.out.println(divider);
+          if (input.equals("bye")) {
+            System.out.println("    See you!");
+          }
+          else {
+          System.out.println("    " +input);
+          }
+          System.out.println(divider);
+        }
     }
 }
