e4116a2abc84dc9d8cceae3bd203383c6d6f3205
samthambad <98635284+samthambad@users.noreply.github.com>
2025-08-16 21:04:19 +0800

    Adding class to represent Task

--- src/main/java/Sisyphus.java
+++ src/main/java/Sisyphus.java
@@ -1,79 +1,108 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-public class Sisyphus {
-  static String divider = "-----------------------------";
-  public static void printList(ArrayList<String> todoList, ArrayList<Boolean> doneList) {
-    for (int i = 0; i < todoList.size(); i++) {
-      int listIndex = i + 1;
-      if (doneList.get(i)) {
-        System.out.println("    " + listIndex + ". [X] " + todoList.get(i));
-      } else {
-        System.out.println("    " + listIndex + ". [ ] " + todoList.get(i));
-      }
-    }
-  }
 
-  public static void introMessage() {
-    String logo = """
-    ░▒▓███████▓▒░▒▓█▓▒░░▒▓███████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓███████▓▒░ 
-    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
-    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
-    ░▒▓██████▓▒░░▒▓█▓▒░░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓███████▓▒░░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░  
-          ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░ 
-          ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░ 
-    ░▒▓███████▓▒░░▒▓█▓▒░▒▓███████▓▒░   ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓███████▓▒░  
-      """;
-    String welcomeMessage = "Hello, I am Sisyphus, what can I do for you?";
-    System.out.println(logo);
-    System.out.println(welcomeMessage);
-    System.out.println(divider);
-  }
+public class Sisyphus {
+    public static class Task {
+        private String name;
+        private Boolean isDone = false;
+
+        public Task(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public String toString() {
+            if (isDone) {
+                return "[X] " + this.name;
+            } else {
+                return "[ ] " + this.name;
+            }
+        }
+
+        public void complete() {
+            this.isDone = true;
+        }
+
+        public void incomplete() {
+            this.isDone = false;
+        }
+    }
+
+    static String divider = "-----------------------------";
+
+    public static void introMessage() {
+        String logo = """
+                ░▒▓███████▓▒░▒▓█▓▒░░▒▓███████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓███████▓▒░
+                ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░
+                ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░
+                ░▒▓██████▓▒░░▒▓█▓▒░░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓███████▓▒░░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░
+                      ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░
+                      ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░
+                ░▒▓███████▓▒░░▒▓█▓▒░▒▓███████▓▒░   ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓███████▓▒░
+                  """;
+        String welcomeMessage = "Hello, I am Sisyphus, what can I do for you?";
+        System.out.println(logo);
+        System.out.println(welcomeMessage);
+        System.out.println(divider);
+    }
+
     public static void main(String[] args) {
         introMessage();
-        ArrayList<String> todoList = new ArrayList<String>();
-        ArrayList<Boolean> doneList = new ArrayList<>();
+        ArrayList<Task> todoList = new ArrayList<>();
         String input = "";
         Scanner myObj = new Scanner(System.in);
         while (!input.equals("bye")) {
-          input = myObj.nextLine();
-          String[] inputArr = input.split(" ");
-          System.out.println(divider);
-          switch (inputArr[0]) {
-            case "bye":
-              System.out.println("    See you!");
-              break;
-            case "list":
-              printList(todoList, doneList);
-              break;
-            case "mark":
-              if (inputArr[1].length() > 0 && inputArr[1] <= todoList.size()) {
-                int number = Integer.parseInt(inputArr[1]);
-                doneList.set(number-1, true);
-                System.out.println("Okay, task " + number + " is done");
-                printList(todoList, doneList);
-              } else {
-                System.out.println("Task " + inputArr[1] + " does not exist")
-              }
-              break;
-            case "unmark":
-              if (inputArr[1].length() > 0 && inputArr[1] <= todoList.size()) {
-                int number = Integer.parseInt(inputArr[1]);
-                doneList.set(number-1, false);
-                System.out.println("Okay, task " + number + " is not done yet");
-                printList(todoList, doneList);
-              }
-              } else {
-                System.out.println("Task " + inputArr[1] + " does not exist")
-              }
-              break;
-            default:
-              if (input.length() == 0) break;
-              todoList.add(input);
-              doneList.add(false);
-              System.out.println("    added: " + input);
-              break;
-          }
-          System.out.println(divider);
+            input = myObj.nextLine();
+            String[] inputArr = input.split(" ");
+            System.out.println(divider);
+            switch (inputArr[0]) {
+                case "bye":
+                    System.out.println("    See you!");
+                    break;
+                case "list":
+                    for (int i = 0; i < todoList.size(); i++) {
+                        int listIdx = i + 1;
+                        System.out.println("    " + listIdx + "." + todoList.get(i));
+                    }
+                    break;
+                case "mark":
+                    if (inputArr[1].length() > 0 && Integer.parseInt(inputArr[1]) <= todoList.size()) {
+                        int number = Integer.parseInt(inputArr[1]);
+                        todoList.get(number - 1).complete();
+                        System.out.println("Okay, task " + number + " is done");
+                        for (int i = 0; i < todoList.size(); i++) {
+                            int listIdx = i + 1;
+                            System.out.println("    " + listIdx + "." + todoList.get(i));
+                        }
+                    } else {
+                        System.out.println("Task " + inputArr[1] + " does not exist");
+                    }
+                    break;
+                case "unmark":
+                    if (inputArr[1].length() > 0 && Integer.parseInt(inputArr[1]) <= todoList.size()) {
+                        int number = Integer.parseInt(inputArr[1]);
+                        todoList.get(number - 1).incomplete();
+                        System.out.println("Okay, task " + number + " is not done yet");
+                        for (int i = 0; i < todoList.size(); i++) {
+                            int listIdx = i + 1;
+                            System.out.println("    " + listIdx + "." + todoList.get(i));
+                        }
+                    } else {
+                        System.out.println("Task " + inputArr[1] + " does not exist");
+                    }
+                    break;
+                default:
+                    if (input.length() == 0)
+                        break;
+                    Task newTask = new Task(input);
+                    todoList.add(newTask);
+                    System.out.println("    added: " + input);
+                    break;
+            }
+            System.out.println(divider);
         }
     }
 }
