a93a0737b72f5801793b62054d00c7a0b1c7ae47
samthambad <98635284+samthambad@users.noreply.github.com>
2025-08-16 16:53:45 +0800

    Added done/not done marking

--- src/main/java/Sisyphus.java
+++ src/main/java/Sisyphus.java
@@ -1,6 +1,16 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 public class Sisyphus {
+  public static void printList(ArrayList<String> todoList, ArrayList<Boolean> doneList) {
+    for (int i = 0; i < todoList.size(); i++) {
+      int listIndex = i + 1;
+      if (doneList.get(i)) {
+        System.out.println("    " + listIndex + ". [X] " + todoList.get(i));
+      } else {
+        System.out.println("    " + listIndex + ". [ ] " + todoList.get(i));
+      }
+    }
+  }
     public static void main(String[] args) {
         String logo = """
         ░▒▓███████▓▒░▒▓█▓▒░░▒▓███████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓███████▓▒░ 
@@ -16,24 +26,42 @@ public class Sisyphus {
         System.out.println(logo);
         System.out.println(welcomeMessage);
         System.out.println(divider);
+
         ArrayList<String> todoList = new ArrayList<String>();
+        ArrayList<Boolean> doneList = new ArrayList<>();
         String input = "";
         Scanner myObj = new Scanner(System.in);
         while (!input.equals("bye")) {
           input = myObj.nextLine();
+          String[] inputArr = input.split(" ");
           System.out.println(divider);
-          switch (input) {
+          switch (inputArr[0]) {
             case "bye":
               System.out.println("    See you!");
               break;
             case "list":
-              int i = 1;
-              for (String item : todoList) {
-                System.out.println("    " + i++ + ". " + item);
+              printList(todoList, doneList);
+              break;
+            case "mark":
+              if (inputArr[1].length() > 0) {
+                int number = Integer.parseInt(inputArr[1]);
+                doneList.set(number-1, true);
+                System.out.println("Okay, task " + number + " is done");
+                printList(todoList, doneList);
+              }
+              break;
+            case "unmark":
+              if (inputArr[1].length() > 0) {
+                int number = Integer.parseInt(inputArr[1]);
+                doneList.set(number-1, false);
+                System.out.println("Okay, task " + number + " is not done yet");
+                printList(todoList, doneList);
               }
               break;
             default:
+              if (input.length() == 0) break;
               todoList.add(input);
+              doneList.add(false);
               System.out.println("    added: " + input);
               break;
           }
